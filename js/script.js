289c1fffb7b810249f3928c35189b41230712316
stian-git <82411646+stian-git@users.noreply.github.com>
2022-10-10 09:14:37 +0200

    Started on header and footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Test");
+import { body, headerOffsetTop } from "./components/variables.js";
+
+// Adds the sticky-class when the navbar should be locked to the top:
+window.onscroll = () => {
+    if (window.pageYOffset > headerOffsetTop) {
+        //header.classList.add("sticky");
+        body.classList.add("navbar-present");
+    } else {
+        //header.classList.remove("sticky");
+        body.classList.remove("navbar-present");
+    }
+};
