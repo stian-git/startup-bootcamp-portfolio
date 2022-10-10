1d9f59396cc33d81fae981ecb8d509c8a900e2de
stian-git <82411646+stian-git@users.noreply.github.com>
2022-10-10 15:02:57 +0200

    SBP-11, SBP-12 - Header and navbar

--- js/components/variables.js
+++ js/components/variables.js
@@ -1,5 +1,5 @@
 export const navbar = document.querySelector(".header__navbar");
-export const main = document.querySelector("main");
+//export const main = document.querySelector("main");
 export const body = document.querySelector("body");
 //export const header = document.querySelector("header");
 export const headerOffsetTop = navbar.offsetTop;
