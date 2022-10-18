b1f1d229e09e3b4b25fabb99b35c46586e80726c
stian-git <82411646+stian-git@users.noreply.github.com>
2022-10-18 15:29:09 +0200

    SBP-45 - Show required fields in contact form.

--- js/script.js
+++ js/script.js
@@ -71,3 +71,7 @@ formNameField.addEventListener("keyup", () => {
 
 showProjects();
 showExperience();
+
+// enable tooltips:
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
