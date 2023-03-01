46f719756d40a6e9bd0a0b4e437c335127eab6e7
stian-git <82411646+stian-git@users.noreply.github.com>
2023-03-01 18:33:23 +0100

    Hide live demo link when unavailable.

--- js/components/showProjects.js
+++ js/components/showProjects.js
@@ -2,22 +2,28 @@ import toggleVisibleProject from "./toggleVisibleProject.js";
 import { portfolioSection } from "./variables.js";
 
 export default function showProjects(projects) {
-    portfolioSection.innerHTML = `<h1 class="portfolio">Portfolio</h1>`;
-    projects.forEach((project, projectindex) => {
-        const keywordsString = project.keywords.join(", ") + ".";
-        let highlightsList = "";
-        project.highlights.forEach((highlight) => {
-            highlightsList += `<li class="project__row-listitem">${highlight}</li>`;
-        });
-        let description = "";
-        project.description.forEach((line) => {
-            description += `<p class="project__row-body">${line}</p>`;
-        });
-        let repoIconClasses = "fa-solid fa-code";
-        if (project.repositoryType.toLowerCase() === "github") {
-            repoIconClasses = "fa-brands fa-github";
-        }
-        portfolioSection.innerHTML += `
+  portfolioSection.innerHTML = `<h1 class="portfolio">Portfolio</h1>`;
+  projects.forEach((project, projectindex) => {
+    const keywordsString = project.keywords.join(", ") + ".";
+    let highlightsList = "";
+    project.highlights.forEach((highlight) => {
+      highlightsList += `<li class="project__row-listitem">${highlight}</li>`;
+    });
+    let description = "";
+    project.description.forEach((line) => {
+      description += `<p class="project__row-body">${line}</p>`;
+    });
+    let repoIconClasses = "fa-solid fa-code";
+    if (project.repositoryType.toLowerCase() === "github") {
+      repoIconClasses = "fa-brands fa-github";
+    }
+    let externalLinkMobile = "";
+    let externalLink = "";
+    if (project.url) {
+      externalLinkMobile = `<a href="${project.url}" target="_blank" title="Link to ${project.title}" aria-label="Link to ${project.title}"><i class="fa-solid fa-share-from-square fa-2xl project__row-icon project__row-icon-mobile"></i></a>`;
+      externalLink = `<a href="${project.url}" target="_blank" title="Link to ${project.title}" aria-label="Link to ${project.title}"><i class="fa-solid fa-share-from-square fa-2xl project__row-icon"></i></a>`;
+    }
+    portfolioSection.innerHTML += `
         <div class="project">
                     <div class="project__row">
                         <div class="project__row-left">
@@ -37,7 +43,7 @@ export default function showProjects(projects) {
                         </div>
                         <div class="project__row-right project-desktoponly">
                             <div class="project__row-right-top">
-                                <i class="fa-solid fa-magnifying-glass fa-2xl project__row-icon project__row-icon-magnifier" data-projectid="${project.id}" data-projectindex="${projectindex}"></i><a href="${project.url}" target="_blank" title="Link to ${project.title}" aria-label="Link to ${project.title}"><i class="fa-solid fa-share-from-square fa-2xl project__row-icon"></i></a>
+                                <i class="fa-solid fa-magnifying-glass fa-2xl project__row-icon project__row-icon-magnifier" data-projectid="${project.id}" data-projectindex="${projectindex}"></i>${externalLink}
                             </div>
                         </div>
                     </div>
@@ -67,30 +73,30 @@ export default function showProjects(projects) {
                         <div class="project__row-right-top">
                             <p class="project__row-header project-mobileonly project-mobileonly-left">Actions:</p>
                             <i class="fa-solid fa-magnifying-glass fa-2xl project__row-icon project__row-icon-magnifier project__row-icon-mobile" data-projectid="${project.id}" data-projectindex="${projectindex}"></i>
-                            <a href="${project.url}" target="_blank" title="Link to ${project.title}" aria-label="Link to ${project.title}"><i class="fa-solid fa-share-from-square fa-2xl project__row-icon project__row-icon-mobile"></i></a>
+                            ${externalLinkMobile}
                         </div>
                     </div>
                 </div>`;
-    });
-    // add active class to the first project:
-    document.querySelectorAll(".project")[0].classList.add("active");
-    // Make additional elements visible to the active project.
-    toggleVisibleProject(0);
+  });
+  // add active class to the first project:
+  document.querySelectorAll(".project")[0].classList.add("active");
+  // Make additional elements visible to the active project.
+  toggleVisibleProject(0);
 
-    // Add eventlisteners to magnifying glasses:
-    const allMagnifyingGlasses = document.querySelectorAll(".fa-magnifying-glass");
-    allMagnifyingGlasses.forEach((icon) => {
-        icon.addEventListener("click", toggleVisibleProject);
+  // Add eventlisteners to magnifying glasses:
+  const allMagnifyingGlasses = document.querySelectorAll(".fa-magnifying-glass");
+  allMagnifyingGlasses.forEach((icon) => {
+    icon.addEventListener("click", toggleVisibleProject);
+  });
+  // Add eventlisteners to images for modal toggling:
+  const allProjectImages = document.querySelectorAll(".project__row-image");
+  const modalImage = document.querySelector(".imagemodal__body-message-image");
+  const imageModalTrigger = document.querySelector(".imagemodal-trigger");
+  allProjectImages.forEach((thumb) => {
+    thumb.addEventListener("click", (e) => {
+      const fullsizeUrl = e.target.dataset.fullsize;
+      modalImage.src = fullsizeUrl;
+      imageModalTrigger.click();
     });
-    // Add eventlisteners to images for modal toggling:
-    const allProjectImages = document.querySelectorAll(".project__row-image");
-    const modalImage = document.querySelector(".imagemodal__body-message-image");
-    const imageModalTrigger = document.querySelector(".imagemodal-trigger");
-    allProjectImages.forEach((thumb) => {
-        thumb.addEventListener("click", (e) => {
-            const fullsizeUrl = e.target.dataset.fullsize;
-            modalImage.src = fullsizeUrl;
-            imageModalTrigger.click();
-        });
-    });
+  });
 }
